17de954d1ce01bee60040fc8c51932fe7fdb2a3b
Peter1907 <peter.nady.1408@gmail.com>
2022-11-29 17:34:21 +0200

    Add Item class & main.rb file

---
+++ item.rb
@@ -0,0 +1,38 @@
+class Item
+  def initialize(publish_date, archived: false)
+    @id = rand[0..100]
+    @genre = []
+    @author = []
+    @source = []
+    @label = []
+    @publish_date = publish_date
+    @archived = archived
+  end
+
+  def add_genre(genre)
+    @genre << Genre.new(genre)
+  end
+  
+  def add_author(author)
+    @author << Author.new(author)
+  end
+
+  def add_source(source)
+    @source << Source.new(source)
+  end
+
+  def add_label(label)
+    @label << Label.new(label)
+  end
+
+  def can_be_archived?
+    return true if @publish_date < (Time.now.year - 10).to_s
+    return false
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+
+  private :can_be_archived?
+end
---
+++ main.rb
@@ -0,0 +1,31 @@
+$menu = <<MLS
+1. List all books
+2. List all music albums
+3. List all movies
+4. List all games
+5. List all genres
+6. List all labels
+7. List all authors
+8. List all sources
+9. Add a book
+10. Add a music album
+11. Add a movie
+12. Add a game
+13. Exit
+MLS
+
+class Main
+  def initialize
+    puts "Welcome to the Media Library"
+    loop do
+      puts $menu
+      input = gets.chomp.to_i
+      if input == 13
+        puts "Thank you for using the Media Library"
+        break
+      end
+    end
+  end
+end
+
+Main.new
