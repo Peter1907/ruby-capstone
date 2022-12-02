4a4f2ed663ba8342d686a2f208fb456fe98491d1
wadaudzai <wadaudzai@gmail.com>
2022-12-02 15:22:42 -0500

    Fix linter errors.

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -2,8 +2,6 @@ require_relative '../music_album'
 describe MusicAlbum do
   context 'Testing Music album' do
     music_album = MusicAlbum.new('10/10/2010', true, true)
-    music_album2 = MusicAlbum.new('02/02/2020', true, true)
-    music_album3 = MusicAlbum.new('10/10/2010', true, false)
     it 'it creates a music album instance' do
       expect(music_album.publish_date).to eq('10/10/2010')
     end
