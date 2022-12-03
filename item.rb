8bbfd635674e84c96541ebe3109cca88939c925e
wadaudzai <wadaudzai@gmail.com>
2022-12-03 01:07:00 -0500

    Make changes based on reviewers review.

--- item.rb
+++ item.rb
@@ -13,19 +13,23 @@ class Item
   end
 
   def add_genre(genre)
-    @genre << genre
+    @genre = genre
+    genre.items.push(self) unless genre.items.include?(self)
   end
 
   def add_author(author)
-    @author << author
+    @author = author
+    author.items.push(self) unless author.items.include?(self)
   end
 
   def add_source(source)
-    @source << source
+    @source = source
+    source.items.push(self) unless source.items.include?(self)
   end
 
   def add_label(label)
-    @label << label
+    @label = label
+    label.items.push(self) unless label.items.include?(self)
   end
 
   def can_be_archived?
